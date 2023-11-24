cb5f58ea2931e6e7590edbfef00f083b8de0fecb
Owlcityss <151890087+Owlcityss@users.noreply.github.com>
2023-11-24 15:21:40 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -10,17 +10,17 @@ const CONFIG = {
   ],
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: 'visual design',
+  HEO_HERO_TITLE_2: '+++',
+  HEO_HERO_TITLE_3: 'OWLCITY.COM',
+  HEO_HERO_TITLE_4: 'blender++',
+  HEO_HERO_TITLE_5: '探索3D艺术',
+  HEO_HERO_TITLE_LINK: 'https://www.blender.org/',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '作品分享', url: '/tag/必看精选' },
+  HEO_HERO_CATEGORY_2: { title: '学习思考', url: '/tag/热门文章' },
+  HEO_HERO_CATEGORY_3: { title: '更多内容', url: '/tag/实用教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
